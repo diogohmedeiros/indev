11124bd273e392d702743e97eb6e7bdb134b9edf
Ana Júlia Moreira Vieira <a.jlmoreira7@gmail.com>
2022-05-11 07:49:29 -0300

    app stack screen

---
+++ Projeto/mobile/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+import Login from './pages/login';
+import Main from './pages/main';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false,
+      }}>
+        {/* <Stack.Screen name="Login" component={Login} /> */}
+        <Stack.Screen name="Main" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
