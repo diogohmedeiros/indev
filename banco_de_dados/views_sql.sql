9c24a64a55a113efc12bf7d8e26679be7df2d62b
DanielS262 <dansouza2@live.com>
2022-06-07 11:16:12 -0300

    alteracao no script BD

--- banco_de_dados/views_sql.sql
+++ banco_de_dados/views_sql.sql
@@ -31,3 +31,8 @@ CREATE VIEW vw_vaga_02 AS
 SELECT e.nome_empresa, e.cnpj, v.id_empresa, v.id_vaga, v.cidade, v.cargo, v.salario, v.descricao, v.requisitos, v.expediente,
 v.data_de_publicacao, v.data_encerra_vaga, v.email_de_contato, v.status_vaga, b.descricao AS beneficio FROM empresas e 
 inner join vagas v on e.id_empresa = v.id_empresa inner join relac_benef_vaga bv on bv.id_vaga = v.id_vaga inner join beneficios b on bv.id_beneficio = b.id_beneficio;
+
+
+CREATE VIEW vw_enderecos_empresas AS 
+SELECT ed.id_empresa, e.nome_empresa, ed.cep, ed.nome_pais, ed.nome_estado, ed.nome_cidade,
+ed.nome_bairro, ed.nome_rua, ed.numero, ed.complemento FROM enderecos_empresa ed INNER JOIN empresas e ON ed.id_empresa = e.id_empresa;
