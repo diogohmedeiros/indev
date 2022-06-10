89bc1926dfcf8ba6f1287304d7182fafd8630346
Diogo Medeiros <diogohmedeiross@gmail.com>
2022-06-10 11:32:08 -0300

    Atualização 10/06 - 11h27

---
+++ Projeto/web/login/script.js
@@ -0,0 +1,35 @@
+var email = document.getElementById("email");
+var senha = document.getElementById("senha");
+
+var entrar = document.getElementById("entrar").addEventListener("click", () => {
+    fazerLogin();
+})
+
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault(); 
+});
+
+async function fazerLogin(){
+    let body = {
+        email: email.value,
+        senha: senha.value
+    } 
+
+    let settings =  {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }
+
+    let endpoint = "http://localhost:3000/login_usuario";
+
+    let response = await fetch(endpoint, settings);
+    let data = await response.json();
+
+    if(response.status == 200){
+        localStorage.setItem("id_usuario", data.id_usuario);
+        window.location.href = "../home";
+    }
+}
