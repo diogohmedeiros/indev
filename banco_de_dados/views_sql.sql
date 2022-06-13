e5127b0d868f4867d09b8f54a880f047ba0e15b1
DanielS262 <dansouza2@live.com>
2022-06-13 11:20:54 -0300

    documentação projeto

--- banco_de_dados/views_sql.sql
+++ banco_de_dados/views_sql.sql
@@ -36,3 +36,33 @@ inner join vagas v on e.id_empresa = v.id_empresa inner join relac_benef_vaga bv
 CREATE VIEW vw_enderecos_empresas AS 
 SELECT ed.id_empresa, e.nome_empresa, ed.cep, ed.nome_pais, ed.nome_estado, ed.nome_cidade,
 ed.nome_bairro, ed.nome_rua, ed.numero, ed.complemento FROM enderecos_empresa ed INNER JOIN empresas e ON ed.id_empresa = e.id_empresa;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE VIEW vw_vaga_02 AS 
+
+SELECT e.nome_empresa, e.cnpj, v.id_empresa, v.id_vaga, v.cidade, v.cargo, v.salario, v.descricao, v.requisitos, v.expediente,
+v.data_de_publicacao, v.data_encerra_vaga, v.quantidade_de_vagas, v.email_de_contato, v.status_vaga, b.descricao AS beneficio FROM empresas e 
+inner join vagas v on e.id_empresa = v.id_empresa LEFT OUTER JOIN relac_benef_vaga bv on bv.id_vaga = v.id_vaga LEFT OUTER join beneficios b on bv.id_beneficio = b.id_beneficio;
+
+
+
+SELECT e.nome_empresa, v.id_vaga, v.descricao, b.descricao AS beneficio FROM empresas e 
+inner join vagas v on e.id_empresa = v.id_empresa LEFT OUTER JOIN relac_benef_vaga bv on bv.id_vaga = v.id_vaga LEFT OUTER join beneficios b on bv.id_beneficio = b.id_beneficio;
+
+
+
+
+
+
